80393ebe0a40df4278155ad9cb93063f260d4091
Petr Heinz <noobos@gmail.com>
2022-12-10 15:48:46 +0000

    Removed unused extensions, used prettier

--- script.js
+++ script.js
@@ -1,39 +1,38 @@
-const had = [document.querySelector('.had')]
+const had = [document.querySelector(".had")];
 
 function pohniHadem(dolu, doprava) {
-  
-  console.log("Had je na " + had.id)
+  console.log("Had je na " + had.id);
 
-  let x = parseInt(had[0].id.split(":")[0])
-  let y = parseInt(had[0].id.split(":")[1])
-  const idCil = (x + dolu) + ":" + (y + doprava)
-  console.log("Had bude na " + idCil)
+  let x = parseInt(had[0].id.split(":")[0]);
+  let y = parseInt(had[0].id.split(":")[1]);
+  const idCil = x + dolu + ":" + (y + doprava);
+  console.log("Had bude na " + idCil);
 
-  const cil = document.getElementById(idCil)
-  cil.classList.add("had")
-  had.unshift(cil)
-  
-  const posledni = had.pop()
-  posledni.classList.remove("had")
+  const cil = document.getElementById(idCil);
+  cil.classList.add("had");
+  had.unshift(cil);
+
+  const posledni = had.pop();
+  posledni.classList.remove("had");
 }
 
 function pohyb(udalost) {
   if (udalost.which === 37) {
-    console.log('Hade, jdi doleva pls')
-    pohniHadem(0, -1)
+    console.log("Hade, jdi doleva pls");
+    pohniHadem(0, -1);
   }
   if (udalost.which === 38) {
-    console.log('Hade, jdi nahoru pls')
-    pohniHadem(-1, 0)
+    console.log("Hade, jdi nahoru pls");
+    pohniHadem(-1, 0);
   }
   if (udalost.which === 39) {
-    console.log('Hade, jdi doprava pls')
-    pohniHadem(0, 1)
+    console.log("Hade, jdi doprava pls");
+    pohniHadem(0, 1);
   }
   if (udalost.which === 40) {
-    console.log('Hade, jdi dolů pls')
-    pohniHadem(1, 0)
+    console.log("Hade, jdi dolů pls");
+    pohniHadem(1, 0);
   }
 }
 
-document.addEventListener('keydown', pohyb)
+document.addEventListener("keydown", pohyb);
