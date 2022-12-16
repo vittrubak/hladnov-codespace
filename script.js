a619aec9ffe0bef6e667a6ece6d5768c81fcc937
vittrubak <120735972+vittrubak@users.noreply.github.com>
2022-12-16 16:52:56 +0000

    zraniNahodne

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
-function pohniHadem(dolu, doprava) {
-  const had = document.querySelector(".had");
-  console.log("Had je na " + had.id);
+let had = [document.querySelector(".had")]
 
-  let radek = parseInt(had.id.split(":")[0]);
-  let sloupec = parseInt(had.id.split(":")[1]);
+
+function pohniHadem(dolu, doprava) {
+  const hadHlava = had[0]
+  console.log("Had je na " + hadHlava.id);
+
+  let radek = parseInt(hadHlava.id.split(":")[0]);
+  let sloupec = parseInt(hadHlava.id.split(":")[1]);
   const idCil = radek + dolu + ":" + (sloupec + doprava);
   console.log("Had bude na " + idCil);
 
   const cil = document.getElementById(idCil);
+
+  had.unshift(cil); // vloží do pole "cíl" na začátek
+
   cil.classList.add("had");
-  had.classList.remove("had");
-}
+  
+  if (cil.classList.contains("zradlo")) {
+    console.log("had bude zrat")
+    cil.classList.remove("zradlo")
+    const zradloradek = Math.floor(Math.random() * 5+1);
+    const zradlosloupec = Math.floor(Math.random() * 5+1);
+    const idZradlo = zradloradek + ":" + zradlosloupec;
+
+    console.log(idZradlo);
+    const zradlonekde = document.getElementById(idZradlo)
+    zradlonekde.classList.add("zradlo")
+    
+    
+  }
+    else {
+      const polektereprestavabythadem = had.pop();
+      polektereprestavabythadem.classList.remove("had"); }
+      
+  }
 
 function pohyb(udalost) {
   if (udalost.which === 37) {
